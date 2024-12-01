1a0e950319cc3b46b4d67a6e9e4b6a6500af92f9
horsicq <horsicq@gmail.com>
2024-12-02 00:23:49 +0100

    Update file(s): 2024-12-02

--- xdataconvertorwidget.cmake
+++ xdataconvertorwidget.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xdataconvertor.cmake)
+if (NOT DEFINED XDATACONVERTOR_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xdataconvertor.cmake)
+    set(XDATACONVERTORWIDGET_SOURCES ${XDATACONVERTORWIDGET_SOURCES} ${XDATACONVERTOR_SOURCES})
+endif()
 
 set(XDATACONVERTORWIDGET_SOURCES
+    ${XDATACONVERTORWIDGET_SOURCES}
     ${XDATACONVERTOR_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogxdataconvertorprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogxdataconvertorprocess.h
